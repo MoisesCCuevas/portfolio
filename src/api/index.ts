9e45546b357ad12c6e18a8ba4adfd19712a6e3fe
Moisés C. Cuevas <moisesccuevas@gmail.com>
2025-02-11 17:44:24 -0600

    ignore mock

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 const API = axios.create();
 
 export const getData = async () => {
-  const response = await API.get('/mockData.json');
+  const response = await API.get('/mockData/mockData.json');
   return response.data;
 };
